d0144369fd68552549a2fc2a2b1cdbaa1fd71658
Poponta <nakamura.ryota.cx@tut.jp>
2026-02-01 18:34:49 +0900

    （３）calc関数　修正

--- calc_mul.py
+++ calc_mul.py
@@ -3,22 +3,11 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
+        if(isinstance(A, int) and isinstance(B, int)):
+                if 1 <= A <= 999 and 1 <= B <= 999:
+                        return A * B
                 else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
+                        return -1
         else:
                 return -1
         
